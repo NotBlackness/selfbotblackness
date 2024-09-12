6e96c5141addc10a7a6078bfa87a470c0d9a8ff8
NotBlackness <notblackness@gmail.com>
2024-09-12 19:30:57 +0000

    Initial commit

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    token: process.env.token,
+    prefix: ";",
+    allowedUserIDs: ['1153611682250227764', '#', '#'], // Replace '#' with actual user IDs
+    allowedNoPrefixUserIDs: ['1153611682250227764'], // Users allowed to use commands without a prefix
+    ltcAddress: 'LcPnFkTa5UTav5Ue3dM6GdLh7LpTm47JZx',
+    paypalID: 'your-paypal-id@example.com'
+};
